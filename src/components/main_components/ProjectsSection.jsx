674787acf3b9c12d97261ce0010982625a0c2b86
rthaqori <ramanthaqori@gmail.com>
2024-07-22 18:15:38 +0545

    commit

--- src/components/main_components/ProjectsSection.jsx
+++ src/components/main_components/ProjectsSection.jsx
@@ -1,7 +1,8 @@
 import ProjectCard from "../ProjectCard";
-import ProjectOneImage from "../../assets/project1-img.jpg";
-import ProjectTwoImage from "../../assets/project2-img.jpg";
-import ProjectFourImage from "../../assets/AmazonUi.jpg";
+import MultiStepForm from "../../assets/project1-img.jpg";
+import ProductPage from "../../assets/project2-img.jpg";
+import AmazonUI from "../../assets/AmazonUi.jpg";
+import EcommerceSite from "../../assets/eCommerce.jpg";
 
 const ProjectsSection = () => {
   const projects = [
@@ -9,7 +10,7 @@ const ProjectsSection = () => {
       title: "Mulit-step Form",
       description:
         "Designed and developed a multi-step form UI for selecting subscription plans. Focused on creating a user-friendly, intuitive layout that guided users through each step of the subscription process. Ensured the design was responsive and accessible across all devices. Conducted user testing to gather feedback and made iterative improvements, resulting in a smoother user experience and higher subscription completion rates",
-      image: ProjectOneImage,
+      image: MultiStepForm,
       githubLink: "https://github.com/rthaqori/multi-step-form-main",
       liveLink: "https://rthaqori.github.io/multi-step-form-main/",
     },
@@ -17,25 +18,25 @@ const ProjectsSection = () => {
       title: "Product Page",
       description:
         "Independently redesigned the UI for an e-commerce product page, enhancing visual appeal and user experience. Focused on a clean layout, intuitive navigation, and responsive design for optimal use across devices. Conducted usability testing to refine the design, leading to increased user engagement and satisfaction.",
-      image: ProjectTwoImage,
+      image: ProductPage,
       githubLink: "https://github.com/rthaqori/ecommerce-product-page-main",
       liveLink: "https://rthaqori.github.io/ecommerce-product-page-main/",
     },
-    // {
-    //   title: "Project 3",
-    //   description:
-    //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, libero",
-    //   image: ProjectOneImage,
-    //   githubLink: "https://www.google.com",
-    //   liveLink: "https://www.google.com",
-    // },
     {
       title: "Amazon UI",
       description:
         "Independently redesigned Amazon’s product page UI, focusing on a clean, intuitive layout and improved functionality. Implemented responsive design for seamless multi-device use and conducted usability testing to enhance user engagement and satisfaction.",
-      image: ProjectFourImage,
+      image: AmazonUI,
       githubLink: "https://github.com/rthaqori/Amazon",
       liveLink: "https://rthaqori.github.io/Amazon/",
+    },
+    {
+      title: "myEcommerce-site",
+      description:
+        "Designed and developed a responsive e-commerce website with a focus on user-friendly navigation, dynamic product listings, and secure checkout process. Improved site performance and enhanced user engagement through optimized UI/UX design.",
+      image: EcommerceSite,
+      githubLink: "https://github.com/rthaqori/my-ecommerce-site",
+      liveLink: "https://rthaqori.github.io/my-ecommerce-site/",
     },
   ];
 
